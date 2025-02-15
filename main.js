ccb03a70e580297a1c6ada3a0557e2b910e201f6
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-02-15 11:22:48 +0200

    Refactor getAdjacentVertices and knightMoves

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function knightMoves(start, end) {
   if (start[0] === end[0] && start[1] === end[1]) {
-    return{ route: [], len: 0 };
+    return { route: start, len: 0 };
   }
   let routesData = new Map();
   let queue = [];
@@ -11,8 +11,8 @@ function knightMoves(start, end) {
   let initialAdjacentVertices = getAdjacentVertices(start[0], start[1]);
   for (const vertex of initialAdjacentVertices) {
     queue.push({
-      curr: [vertex[0], vertex[1]],
-      prev: [start[0], start[1]],
+      curr: vertex,
+      prev: start,
       len: 1,
     });
   }
@@ -27,10 +27,10 @@ function knightMoves(start, end) {
       if (qVertex.curr[0] === end[0] && qVertex.curr[1] === end[1]) {
         let route = [];
         let routeLen = routesData.get(`${end[0]}:${end[1]}`).len;
-        let iVertex = [end[0], end[1]];
+        let iVertex = end;
         let iVertexPrev = routesData.get(`${iVertex[0]}:${iVertex[1]}`).prev;
         while (iVertex !== null) {
-          route.push([iVertex[0], iVertex[1]]);
+          route.push(iVertex);
           iVertex = iVertexPrev;
           if (iVertex !== null) {
             iVertexPrev = routesData.get(`${iVertex[0]}:${iVertex[1]}`).prev;
@@ -45,8 +45,8 @@ function knightMoves(start, end) {
       for (const vertex of adjacentVertices) {
         if (!routesData.has(`${vertex[0]}:${vertex[1]}`)) {
           queue.push({
-            curr: [vertex[0], vertex[1]],
-            prev: [qVertex.curr[0], qVertex.curr[1]],
+            curr: vertex,
+            prev: qVertex.curr,
             len: qVertex.len + 1,
           });
         }
@@ -59,42 +59,30 @@ function knightMoves(start, end) {
   return routesData;
 }
 
-
 function getAdjacentVertices(x, y) {
-  let ones = [-1, 1];
-  let twos = [-2, 2];
+  let steps = [
+    [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [-2, -1],
+    [-2, 1],
+    [2, -1],
+    [2, 1],
+  ];
   let adjacentVertices = [];
-
-  for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 2; j++) {
-      if (
-        x + ones[i] >= 0 &&
-        x + ones[i] < 8 &&
-        y + twos[j] >= 0 &&
-        y + twos[j] < 8
-      ) {
-        adjacentVertices.push([x + ones[i], y + twos[j]]);
-      }
-    }
-  }
-
-  for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 2; j++) {
-      if (
-        x + twos[i] >= 0 &&
-        x + twos[i] < 8 &&
-        y + ones[j] >= 0 &&
-        y + ones[j] < 8
-      ) {
-        adjacentVertices.push([x + twos[i], y + ones[j]]);
-      }
+  for (const step of steps) {
+    let newX = x + step[0];
+    let newY = y + step[1];
+    if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+      adjacentVertices.push([newX, newY]);
     }
   }
 
   return adjacentVertices;
 }
 
-let routeData = knightMoves([3, 3], [4, 3]);
+let routeData = knightMoves([0, 0], [7, 7]);
 
 console.log(`You made it in ${routeData.len} moves! Here's your path:`);
 for (const vertex of routeData.route) {
